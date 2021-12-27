c67ccf6120f92b1b26f46a943c095cba2c757de5
TrygoDigital <ercapuchino@up.edu.ph>
2021-12-27 18:07:38 +0800

    feat: initial backend and frontend set-up - backend listening to port 4000 - backend dependencies: expressjs, nodemon - frontend using React App

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+require('./router')(app)
+
+app.listen(4000, (err) => {
+  if (err) { console.log(err) }
+  else {console.log('Server started at port 4000')}
+})
