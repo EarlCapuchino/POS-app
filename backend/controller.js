9635b58e15a26dbf38f1987609e4ee0718aabb91
Aimer350 <aaramos1@up.edu.ph>
2022-01-02 06:19:20 +0000

    Feat: Controller add login and setup account

--- backend/controller.js
+++ backend/controller.js
@@ -25,12 +25,98 @@ exports.homepage =  (req, res) =>{ //hompage scree, will check iof there are exi
 
 }
 
-exports.login = async (req, res) => {
-//insert login commands
+exports.login = async (req, res) => { //login for the app
+    //CHANGE THE IMPLEMENTATION OF THIS
+    console.log("LOGIN BACKEND")
+    console.log("input:" +req.body)
+	const { email, password } = req.body
+	const user = await User.findOne({ email }).lean()
+
+    var loginCredentials = { //login credentials to be used by cookies jwt
+        issuer : "capuPOSapp", //the issuer is this software organization
+        subject: email, //the subject is the user's email
+        audience: "http://localhost:3000", //verify this 
+        expiresIn: "24h",
+        algorithm: "RS256" // to emphasize that the program uses private/public keys
+    };
+    
+	if (!user) { //there is no such user
+        MESSAGE = "Invalid email";
+		return res.json({ status: 'error', error: 'Invalid email' })
+	}
+
+	if (await bcrypt.compare(password, user.password)) {
+	// the username, password combination is successful
+
+		const token = jwt.sign(
+			{ //payload
+				id: user._id,
+				username: user.username,
+                email: user.email,
+                role: user.role
+			},
+			privateKey, //refer to private.key
+            loginCredentials //this is how the client proves its identity
+		)
+        console.log("TOKEN: "+ token)
+        MESSAGE="logged in successfully"
+		return res.json({ status: 'ok', token: token }) //the response will be sending data: token to axios which will then save it in the local storage
+	}
+    MESSAGE = "Invalid password"; //user exist but wrong password
+	res.json({ status: 'error', error: 'Invalid username/password' })
 }
 
-exports.setUpAccount = async (req, res) => {
-//insert set up account commands
+exports.setUpAccount = async (req, res) =>{ //setup the account
+    console.log("SET UP INITIAL ACCOUNT")
+    console.log(req.body)
+    console.log(req.body.password)
+    const pwd = req.body.password
+    const password=(await (bcrypt.hash(pwd, 10)))
+    
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: password,
+        role: req.body.role
+    })
+
+    if (req.body.password.length < 8) { //if passwords are less than 8 characters
+		return res.json({
+			status: 'error', error: 'Password should be at least 8 characters'
+		})
+	}
+
+    newUser.save((err) => {
+    if (!err) { //id successfully saved
+
+        var loginCredentials = { //login credentials to be used by cookies jwt
+            issuer : "capuPOSapp", //the issuer is this software organization
+            subject: req.body.email, //the subject is the user's email
+            audience: "http://localhost:3000", //verify this 
+            expiresIn: "24h",
+            algorithm: "RS256" // to emphasize that the program uses private/public keys
+        };
+    
+        const token = jwt.sign(
+            { //payload
+                //id: user._id,
+                username: req.body.username,
+                email: req.body.email, //email used here is assumed to be work email, and not personal email for security purposes
+                role: req.body.role    //it will serve as the unique identifier for our db
+            },
+            privateKey, //refer to private.key
+            loginCredentials //this is how the client proves its identity
+        )
+        console.log("TOKEN: "+ token)
+        return res.json({ status: 'valid', token: token })
+
+    }else if(err.code === 11000){ //check whether an email has been used
+        return res.json({ status: 'error', error: 'email already in use' })
+    }else{ //this is a prompt for duplicate key 
+        console.log("there is an error") 
+        return res.json({status: 'error'})
+    }
+    })
 }
 
 exports.addUser = async (req, res) => {
