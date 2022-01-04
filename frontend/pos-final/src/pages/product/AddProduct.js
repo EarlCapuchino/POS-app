c26fcda1da41f6c6404c0617c03677ad7331281c
TrygoDigital <ercapuchino@up.edu.ph>
2022-01-04 14:50:54 +0800

    fix: frontend homepage background - fix mp4 background - change sizes of fonts

--- frontend/pos-final/src/pages/product/AddProduct.js
+++ frontend/pos-final/src/pages/product/AddProduct.js
@@ -43,7 +43,7 @@ class AddProduct extends React.Component {
 
             <div>
                 <div>
-                <div id="Divider"></div>
+                <div id="Dividerprod"></div>
                     <div>
                         <font size="1000" color= "black" align = "center" text-align = "center">
                         Add Items in Inventory
